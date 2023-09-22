712017f3529bab54e2cc950b06e23fd44e82c754
Salma <aboudou.salma@live.fr>
2023-09-22 12:33:54 +0200

    ajout balises meta CSP

--- kasa_app/src/App.js
+++ kasa_app/src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home.jsx";
 import About from "./pages/About.jsx";
 import NotFound from "./pages/NotFound.jsx";
